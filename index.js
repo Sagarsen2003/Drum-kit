60877f22bcdbdcf43b08875466f8d51f5a272753
Sagar Sengupta <sagarsengupta.cse.tcea.2026@gmail.com>
2024-08-31 21:19:30 +0530

    adding all project files

---
+++ index.js
@@ -0,0 +1,84 @@
+var numberOfDrumBottons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumBottons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+
+
+    });
+}
+
+
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+    
+    buttonAnimation(event.key);
+});
+
+
+function makeSound(key) {
+
+    switch (key) {
+
+        case "w":
+            var audio = new Audio("tom-1.mp3");
+            audio.play();
+            break;
+
+        case "a":
+
+            var audio = new Audio("tom-2.mp3");
+            audio.play();
+            break;
+
+        case "s":
+
+            var audio = new Audio("tom-3.mp3");
+            audio.play();
+            break;
+
+        case "d":
+
+            var audio = new Audio("tom-4.mp3");
+            audio.play();
+            break;
+
+        case "j":
+
+            var audio = new Audio("snare.mp3");
+            audio.play();
+            break;
+
+        case "k":
+
+            var audio = new Audio("crash.mp3");
+            audio.play();
+            break;
+
+        case "l":
+
+            var audio = new Audio("kick-bass.mp3");
+            audio.play();
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout( function () {
+        activeButton.classList.remove("pressed");}, 100);
+
+}
